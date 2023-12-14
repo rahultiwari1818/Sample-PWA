8de198227d7981bcbdbc61941a751533df5f9487
Rahul Tiwari <rahultiwari161189@gmail.com>
2023-12-14 23:19:29 +0530

    created first pwa

---
+++ public/serviceWorker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "version-1";
+const urlsToCache = ["index.html","offline.html"];
+
+
+this.addEventListener("install",(event)=>{
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache)=>{
+            console.log("cache opened");
+            return cache.addAll(urlsToCache);
+        })
+    )
+})
+
+this.addEventListener("fetch",(event)=>{
+    event.respondWith(
+        caches.match(event.request)
+        .then((res)=>{
+            return fetch(event.request)
+            .catch(()=> caches.match(""))
+        })
+    )
+})
+
+
+this.addEventListener("activate",(event)=>{
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+    event.waitUntil(
+        caches.keys()
+        .then((cacheNames)=>Promise.all(
+            cacheNames.map((cacheName)=>{
+                if(!cacheWhiteList.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+})
